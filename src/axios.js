41265a9f9a1b5e7de31d92e5b2aa104fa9545be9
Nikhil <makwananikhil36@gmail.com>
2022-08-23 09:56:23 +0530

    add origin

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,10 @@ const BASE_URL = process.env.REACT_APP_PROD_SERVER_URL;
 
 export const axiosDefault = axios.create({
   baseURL: BASE_URL,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
   withCredentials: true,
 });
